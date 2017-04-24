e1211c3d5ea6c83569ab0c82e277a6e057950d13
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-04-24 14:22:14 +0500

    Fancy output

--- deposit-calc.py
+++ deposit-calc.py
@@ -3,28 +3,25 @@
 
 from optparse import OptionParser
 
-parser = OptionParser()
-parser.add_option("-p", "--bank-percent", dest="bank_percent",
-                  help=u"Годовой процент вашего банка", default=8, type=float)
-parser.add_option("-i", "--initial-money", dest="initial_money",
-                  help=u"Сколько денег у вас есть сейчас чтобы положить в банк",
-                  type=float)
-parser.add_option("-w", "--wage-initial", dest="wage_initial",
-                  help=u"Ваша начальная ежемесячная зарплата", type=float)
-parser.add_option("-W", "--wage-increase", dest="wage_increase",
-                  help=u"Прирост вашей зарплаты в месяц", default=0, type=float)
-parser.add_option("-m", "--monthes", dest="monthes",
-                  help=u"Сколько месяцев планируете держать вклад открытым", default=12,
-                  type=int)
-parser.add_option("-o", "--outcome", dest="outcome",
-                  help=u"Каковы ваши ежемесячные расходы", type=float)
-(options, args) = parser.parse_args()
 
-print options
+def make_options():
+    parser = OptionParser()
 
-
-current_wage = options.wage_initial
-bank_money = options.initial_money
+    parser.add_option("-p", "--bank-percent", dest="bank_percent",
+                      help=u"Годовой процент вашего банка", default=8, type=float)
+    parser.add_option("-i", "--initial-money", dest="initial_money",
+                      help=u"Сколько денег у вас есть сейчас чтобы положить в банк",
+                      type=float)
+    parser.add_option("-w", "--wage-initial", dest="wage_initial",
+                      help=u"Ваша начальная ежемесячная зарплата", type=float)
+    parser.add_option("-W", "--wage-increase", dest="wage_increase",
+                      help=u"Прирост вашей зарплаты в месяц", default=0, type=float)
+    parser.add_option("-m", "--monthes", dest="monthes",
+                      help=u"Сколько месяцев планируете держать вклад открытым", default=12,
+                      type=int)
+    parser.add_option("-o", "--outcome", dest="outcome",
+                      help=u"Каковы ваши ежемесячные расходы", type=float)
+    return parser.parse_args()
 
 
 def wage_tick():
@@ -39,9 +36,18 @@ def tick(money):
     return ((money / 100) * options.bank_percent) / 12
 
 PROFIT = 0
+(options, args) = make_options()
+print options
+current_wage = options.wage_initial
+bank_money = options.initial_money
+
+
 for month in xrange(1, options.monthes + 1):
-    print "month: {0} bank: {1:.2f} wage: {2} profit: {3}".format(month, bank_money, current_wage, tick(bank_money))
-    current_wage = wage_tick()
+    if options.wage_increase == 0:
+        print "month: {0}\tbank: {1:.2f}\tprofit: {2}".format(month, bank_money, tick(bank_money))
+    else:
+        print "month: {0}\tbank: {1:.2f}\twage: {2}\tprofit: {3}".format(month, bank_money, current_wage, tick(bank_money))
+        current_wage = wage_tick()
     PROFIT += tick(bank_money)
     bank_money += tick(bank_money) + income(current_wage)
 
